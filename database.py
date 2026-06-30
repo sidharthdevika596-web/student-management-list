a9801dadd76922d7b4b7691cfc2013eea0bb2c0b
Devika.k <sidharthdevika596@gmail.com>
2026-06-30 22:02:51 +0530

    Update DATABASE_URL for database connection

--- database.py
+++ database.py
@@ -1,7 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "postgresql://postgres:neeraja052007@localhost:5432/student_management"
+DATABASE_URL = "postgresql://postgres:database@123@localhost:5432/student_management"
 
 engine = create_engine(DATABASE_URL)
 
